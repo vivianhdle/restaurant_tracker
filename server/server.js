31b2dd40a8e196b9333d4c772e55be7d46372520
Vivian Le <vivianhdle@gmail.com>
2019-06-25 15:46:08 -0700

    added cron job

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mysql = require('mysql');
 const mysqlcredentials = require('./mysqlcreds.js');
+mysqlCredentials.multipleStatements = true;
 const cors  = require('cors')
+const cron = require('node-cron');
 const db = mysql.createConnection(mysqlcredentials);
 
 
@@ -54,6 +56,31 @@ server.post('/api/update-destination',(req,res,next)=>{
         }
     })
 });
+
+const cronNewTable = cron.schedule('0 * * * *', () => {
+    console.log("cronNewTable called");
+    const query = "CREATE TABLE `travel_wishlist_new` LIKE `travel_wishlist`; RENAME TABLE `travel_wishlest` TO `travel_wishlist_old`, `travel_wishlist_new` TO `travel_wishlist`; INSERT INTO `destination` (`id`, `destination`, `country`, `knownFor`, `mustEat`, `mustDo`) VALUES(1, 'Banff', 'Canada', 'Nature', 'Poutine, maple syrup', 'Camping, hiking, sight seeing'),(2, 'Hong Kong', 'China', 'Cities', 'Chinese', 'Night markets, sight seeing'),(3, 'Pulau Ujong', 'Singapore', 'Business', 'Chinese', 'Gardens by the bay, night mark'),(4, 'Jasper National Park', 'Canada', 'Nature', 'Evil Dave\'s Grill', 'Hiking, camping, sight seeing'),(5, 'Bangkok', 'Thailand', 'Elephants', 'Thai', 'Sight seeing, massages, elepha'),(6, 'New York City', 'United States', 'City life', 'Bagels, pizza', 'Sight seeing, broadway'),(8, 'Hawaii', 'United States', 'Beaches', 'Poke', 'Hiking, beaches, sight seeing'),(9, 'Madrid', 'Spain', 'Music,dance,culture', 'Paella', 'Sight seeing, night life, club'),(10, 'Wellington', 'New Zealand', 'Nature', 'Lamb, Paua', 'Extreme outdoor activities')";
+    db.query(query, (error) => {
+        if(error){
+            console.error('Cron create table error', error);
+        }
+    });
+});
+cronNewTable.start();
+
+const cronDropOldTable = cron.schedule('10 * * * *', () => {
+    console.log("cronDropTable called");
+    const dropOldTableQuery = "DROP TABLE IF EXISTS `travel_wishlist_old`";
+    db.query(dropOldTableQuery, error => {
+        if(error){
+            console.error('Cron drop table error', error);
+        }
+    });
+    
+});
+cronDropOldTable.start();
+
+
 
 server.use((error,req,res,next)=>{
     const output = {
